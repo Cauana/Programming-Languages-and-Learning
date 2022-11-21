246594936738739c26b0e21b8874ee9f0b42f075
Cauana Dias <cauana.dias@hotmail.com>
2022-11-21 17:14:39 -0300

    snake

---
+++ Snake/Snake.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+largura = 640
+altura = 480
+x = largura/2
+y = 0
+tela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Jogo Snake')
+relogio = pygame.time.Clock()
+
+while True:
+    relogio.tick(100)
+    tela.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+            
+    pygame.draw.rect(tela,(255,0,0), (x,y,20,20))
+    if y >= altura:
+        y=0
+    y = y+1
+    
+    pygame.display.update()
