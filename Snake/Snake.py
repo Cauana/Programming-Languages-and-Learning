66f941c95c0f197b1e3cf04a0a592fbfe8654962
Cauana Dias <cauana.dias@hotmail.com>
2022-11-22 02:10:34 -0300

    snake game

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,28 +1,95 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
+music_moeda = pygame.mixer.Sound('Snake\smw_coin.wav')
+music_moeda.set_volume(0.9)
 largura = 640
 altura = 480
-x = largura/2
-y = 0
+x_cobra = int(largura/2)
+y_cobra = int(altura/2)
+velocidade = 15
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption('Jogo Snake')
 relogio = pygame.time.Clock()
 
+def aumenta_cobra(lista_cobra):
+    for xey in lista_cobra:
+        pygame.draw.rect(tela,(0,255,0), (xey[0], xey[1], 20, 20))
+    
+
+
+pontos = 0
+fonte = pygame.font.SysFont('arial',40,True,True)
+lista_cobra = []
+comp_inicial = 5
+x_maca = randint(40,600)
+y_maca = randint(50,430)
+x_controle = 20
+y_controle = 0
 while True:
-    relogio.tick(100)
+    relogio.tick(20)
     tela.fill((0,0,0))
+    
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem,True,(255,255,255))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            exit()
-            
-    pygame.draw.rect(tela,(255,0,0), (x,y,20,20))
-    if y >= altura:
-        y=0
-    y = y+1
+            exit()   
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle= -velocidade 
+                    y_controle = 0  
+                
+            if event.key == K_d:
+                if x_controle == -velocidade: 
+                    pass
+                else:
+                        x_controle = velocidade 
+                        y_controle = 0    
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:  
+                    x_controle= 0 
+                    y_controle =-velocidade 
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    x_controle= 0
+                    y_controle = velocidade        
+    x_cobra += x_controle
+    y_cobra += y_controle
     
+    cobra = pygame.draw.rect(tela,(0,255,0), (x_cobra,y_cobra,20,20))
+    maca = pygame.draw.rect(tela,(255,0,0), (x_maca,y_maca,20,20))
+    
+    if cobra.colliderect(maca):
+        x_maca = randint(40,600)
+        y_maca = randint(50,430)
+        pontos +=1
+        music_moeda.play()
+        comp_inicial +=1
+    
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+
+    lista_cobra.append(lista_cabeca)
+    
+    if len(lista_cobra)>comp_inicial:
+        del lista_cobra[0]
+    
+    aumenta_cobra(lista_cobra)
+    
+    
+    tela.blit(texto_formatado,(30,30))
     pygame.display.update()
